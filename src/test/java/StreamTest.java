7be6634a34d46f29e275eeb8c4c13ffd98b543db
WuLieQing <532131417@qq.com>
2020-05-13 15:15:22 +0800

    学习Stream中的与查找功能相关的find函数

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -20,19 +20,19 @@ public class StreamTest {
      * filter方法联系
      * 将名字大于三个字的电影过滤出来
      */
-    public void testFilter(){
+    public void testFilter() {
 
         //传统方法循环
         List<String> newMovies = new LinkedList<>();
         for (String movie : movies) {
-            if(movie.length()>3)
+            if (movie.length() > 3)
                 newMovies.add(movie);
         }
         StreamUtil.printList(newMovies);
 
         //Stream流的filter方法
         StreamUtil.printList(movies.stream()
-                .filter(m->m.length()>3)        //不满足条件的会被过滤掉
+                .filter(m -> m.length() > 3)        //不满足条件的会被过滤掉
                 .collect(Collectors.toList()));  //重新返回List);
     }
 
@@ -53,7 +53,7 @@ public class StreamTest {
 
         //map方法
         StreamUtil.printList(movies.stream()
-                .map(m->m+"大电影")    //每次将返回的对象作为新值替代原对象
+                .map(m -> m + "大电影")    //每次将返回的对象作为新值替代原对象
                 .collect(Collectors.toList())
         );
     }
@@ -67,7 +67,7 @@ public class StreamTest {
         //将数组转换为Stream
         Integer[] nums = new Integer[]{12, 45, 365, 456, 12,};
         Stream<Integer> stream = Stream.of(nums);  //使用Stream.of方法
-        Stream<String> stream1 = Stream.of("你好","不好","还好");  //或直接用多个对象生成
+        Stream<String> stream1 = Stream.of("你好", "不好", "还好");  //或直接用多个对象生成
 
 
         //将集合对象转换为Stream,调用集合的.stream方法
@@ -86,8 +86,8 @@ public class StreamTest {
         //先根据id正序再根据年龄正序
         users.stream().sorted(
                 Comparator.comparingInt(User::getId)
-                .thenComparingInt(User::getAge))
-                .forEach(u-> System.out.println(u));
+                        .thenComparingInt(User::getAge))
+                .forEach(u -> System.out.println(u));
         System.out.println("---------------------------------------------------");
 
         //先根据id正序再根据年龄倒序(特别注意reverse会对上方所有比较都生效,所以第一个id被倒两次变正,年龄倒一次)
@@ -96,7 +96,8 @@ public class StreamTest {
                         .reversed()
                         .thenComparingInt(User::getAge)
                         .reversed()
-        ).forEach(u-> System.out.println(u));;
+        ).forEach(u -> System.out.println(u));
+        ;
         System.out.println("---------------------------------------------------");
 
         //先根据id倒序再根据年龄正序
@@ -104,7 +105,8 @@ public class StreamTest {
                 Comparator.comparingInt(User::getId)
                         .reversed()
                         .thenComparingInt(User::getAge)
-        ).forEach(u-> System.out.println(u));;
+        ).forEach(u -> System.out.println(u));
+        ;
         System.out.println("---------------------------------------------------");
 
         //先根据id倒序再根据年龄倒序
@@ -112,7 +114,8 @@ public class StreamTest {
                 Comparator.comparingInt(User::getId)
                         .thenComparingInt(User::getAge)
                         .reversed()
-        ).forEach(u-> System.out.println(u));;
+        ).forEach(u -> System.out.println(u));
+        ;
 
     }
 
@@ -128,10 +131,10 @@ public class StreamTest {
         List<String> data = StreamUtil.init();
 
         //使用map方法
-        data.stream().map(d -> Arrays.stream( d.split(""))).forEach(System.out::println);
+        data.stream().map(d -> Arrays.stream(d.split(""))).forEach(System.out::println);
 
         //使用floatMap方法,会将二级流全部平铺
-        data.stream().flatMap(d -> Arrays.stream( d.split("")) //将拆分后的字符串数组转为流返回
+        data.stream().flatMap(d -> Arrays.stream(d.split("")) //将拆分后的字符串数组转为流返回
         ).forEach(System.out::println);
     }
 
@@ -151,7 +154,7 @@ public class StreamTest {
 
         //使用forEach
         data.stream().forEach(d -> System.out.println("哈哈哈")); //类似peek,但是peek是返回流,可以进行下一步操作,
-                                // fooEach没有返回值,会直接结束流,两者看情况使用
+        // fooEach没有返回值,会直接结束流,两者看情况使用
     }
 
     @Test
@@ -167,7 +170,7 @@ public class StreamTest {
         StreamUtil.printList(users);
 
         //是否所有用户的年龄都大于20,使用allMatch
-        System.out.println( stream.allMatch(u -> u.getAge() > 20));
+        System.out.println(stream.allMatch(u -> u.getAge() > 20));
 
         //一个流经历一系列操作之后会自动关闭,所有像上面一样先初始化再使用,下面再使用就会报流已关闭的错误,须注意
 
@@ -175,7 +178,39 @@ public class StreamTest {
         System.out.println(users.stream().anyMatch(u -> u.getAge() > 20));
 
         //是否不存在用户年龄小于10岁,不存在使用noneMatch,有点像anyMatch的对立面
-        System.out.println(users.stream().noneMatch(u->u.getAge()<10));
+        System.out.println(users.stream().noneMatch(u -> u.getAge() < 10));
+
+
+    }
+
+    @Test
+    /**
+     * 学习Stream中的与查找功能相关的find函数
+     * 学习Optional类的使用
+     */
+    public void TestFind() {
+        //可以先用filter对流进行过滤
+
+        List<User> users = StreamUtil.initUser();
+
+        //查找年龄大于20岁的第一个用户,使用findFirst,返回的是一个Optional<T>的对象
+        Optional<User> user = users.stream().filter(u -> u.getAge() > 20).findFirst();
+
+        //Optional相对直接返回实体更加灵活,比如使用get()获取里面保存的对象,若为空则报错
+
+        //可以先判断是否存在对象
+        if(user.isPresent()){
+            //可以自定义如果存在对象则进行什么操作
+            user.ifPresent(o-> System.out.println("存在对象进行的操作"+o));
+        }
+
+        //可以给对象进行默认值初始化,避免为空时的错误,当无值则使用默认值
+        System.out.println(user.orElse(new User(0, "默认名", 88)));
+
+        //有点类似findFirst,findAny并不是随机地选一个，
+        // 如果是数据较少，串行地情况下，一般会返回第一个结果，如果是并行的情况，那就不能确保是第一个。
+        Optional<User> optionalUser = users.stream().filter(u -> u.getAge() > 20).findAny();
+        System.out.println(optionalUser.get());
 
 
     }
