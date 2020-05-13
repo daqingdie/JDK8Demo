e8107ef40ccb47f5d4923096c28c9a1b98cb9524
WuLieQing <532131417@qq.com>
2020-05-13 10:34:24 +0800

    添加初始化用户工具函数

--- src/main/java/Stream/StreamUtil.java
+++ src/main/java/Stream/StreamUtil.java
@@ -1,5 +1,8 @@
 package Stream;
 
+import POJO.User;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,6 +15,16 @@ public class StreamUtil {
         movies.add("哆啦A梦");
         movies.add("蜡笔小新");
         return movies;
+    }
+
+    public static  List<User> initUser(){
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "小王", 18));
+        users.add(new User(2, "小强", 22));
+        users.add(new User(3, "小白", 16));
+        users.add(new User(4, "小黑", 38));
+        users.add(new User(5, "小绿", 22));
+        return users;
     }
 
     public static <T> void printList(List<T> list){
